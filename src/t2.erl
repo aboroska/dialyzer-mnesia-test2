6bfc9d6b1dea633f01c793b5e2c68e48bc01781f
Boroska András <Andras.Peter.Boroska@otpbank.hu>
2017-11-26 16:55:18 +0100

    Add mnesia:create_table/2. No dialyzer warning.

--- src/t2.erl
+++ src/t2.erl
@@ -1,12 +1,26 @@
 -module(t2).
 
 %% API exports
--export([]).
+-export([test/0]).
+
+-define(T, test).
+-record(?T, { key :: integer(), val :: any()}).
 
 %%====================================================================
 %% API functions
 %%====================================================================
-
+-spec test() -> ok | {error, any()}.
+test() ->
+  Options = [{type, set}
+            ,{ram_copies, node()}
+            ,{attributes, record_info(fields, ?T)}
+            ],
+  case mnesia:create_table(?T, Options) of
+    {atomic, ok} ->
+      ok;
+    Error ->
+      {error, Error}
+  end.
 
 %%====================================================================
 %% Internal functions
